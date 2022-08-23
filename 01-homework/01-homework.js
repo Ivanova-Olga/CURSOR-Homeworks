5f02ad4d7f5e5d65547b2409f03969db278be583
DESKTOP-1861NPH\olga <dp010386ioe@gmail.com>
2022-08-23 22:38:03 +0100

    add changes and delete extra strings

--- 01-homework/01-homework.js
+++ 01-homework/01-homework.js
@@ -2,24 +2,28 @@ let potato = 15.678;
 let tomato = 123.965;
 let carrot = 90.2345;
 
-console.log(Math.max(potato, tomato, carrot));
+let max = Math.max(potato, tomato, carrot);
+console.log(max);
 
-console.log(Math.min(potato, tomato, carrot));
+let min = Math.min(potato, tomato, carrot);
+console.log(min);
 
 let summary = potato+tomato+carrot;
 console.log(summary);
 
-console.log(Math.floor(potato)+Math.floor(tomato+Math.floor(carrot)));
+let sum_minus_coins = Math.floor(potato)+Math.floor(tomato)+Math.floor(carrot);
+console.log(sum_minus_coins);
 
-console.log(Math.round(summary / 100)*100);
+let sumround100 = Math.round(summary / 100)*100;
+console.log(sumround100);
 
-let x = Math.floor(summary);
+let boolean = Math.floor(summary);
 if (summary % 2 === 0) {
-    x = true;
+    boolean = true;
 } else {
-    x = false;
+    boolean = false;
 };
-console.log(x);
+console.log(boolean);
 
 let change = 500 - summary;
 console.log(change);
@@ -36,12 +40,15 @@ console.log(charge.toFixed(2));
 let income = summary / 2 - discount;
 console.log(income);
 
-
-let str = `строка на
-несколько строчек,
-можно сколько угодно`
-
-    `Дважды два равно ${2 * 2}`;
-console.log(str);
-
-
+let advanced =  `Максимальна ціна: ${max}
+Мінімальна ціна: ${min}
+Вартість всіх товарів: ${summary}
+Вартість товарів без копійок: ${sum_minus_coins}
+Сума товарів, округлена до сотень: ${sumround100}
+Парне чи не парне число сума всіх товарів: ${boolean}
+Решта з 500 грн: ${change}
+Середнє значення цін, округлене до другого знаку після коми: ${average}
+Випадкова знижка: ${discount}
+Сума до оплати зі знижкою: ${change}
+Чистий прибуток при оплаті зі знижкою: ${income}`;
+console.log(advanced);
